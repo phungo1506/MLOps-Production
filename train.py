6233c0688fa5671b29fc9de393c8cc1e36618440
phungo1506 <18521243@gm.uit.edu.vn>
2024-06-27 23:12:49 +0700

    modified train.py

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-from utils import data_setup, engine_quant, save
+from utils import data_setup, engine, engine_quant, save
 from architecture import Mobilenet, Resnet
 from argparse import ArgumentParser
 import torch
@@ -60,10 +60,10 @@ if __name__ == "__main__":
     optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, momentum=0.9)
 
     # Set the seeds
-    engine_quant.set_seeds()
+    engine.set_seeds()
     print(devices)
     # Train the model and save the training results to a dictionary
-    results = engine_quant.train(model=model,
+    results = engine.train(model=model,
                         train_dataloader=train_dataloader,
                         test_dataloader=test_dataloader,
                         optimizer=optimizer,
